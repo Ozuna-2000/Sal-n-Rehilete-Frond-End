ff47e1e1fffc16b4cb1a0d1f976516908614d474
Jorge Octavio Guzmán Sánchez <jogs78@gmail.com>
2024-12-04 19:25:51 -0600

    Cambios

--- src/Apis/api.js
+++ src/Apis/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import store from '@/store'
 
-const url = 'http://127.0.0.1:8000'
+const url = 'https://back.rehilete.mx'
 // Falta indicar poner el aplicasion=>json
 export const crearEntrada = async (data) => {
   try {
